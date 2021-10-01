5e3ccc6f0c6f87e0f80e2cb1ecbbbd08521da1ce
ikkko <lancial.antoine@gmail.com>
2021-10-01 17:01:26 +0200

    Le PROJET

---
+++ Data.py
@@ -0,0 +1,27 @@
+meetings = { 
+    "1234": {
+        "id": "U01LN94LNPM", 
+        "guestName": "Michel", 
+        "floor": 4
+    },
+    "2222": {
+        "id": "U7ZS2LT8S", 
+        "guestName": "René", 
+        "floor": 2
+    },
+    "2341": {
+        "id": "U01F65ZL5G9", 
+        "guestName": "Duc", 
+        "floor": 3
+    },
+    "8261": {
+        "id": "U020CG63GMV", 
+        "guestName": "Hervé", 
+        "floor": 1
+    },
+    "0837": {
+        "id": "U025S60F42E", 
+        "guestName": "Billy", 
+        "floor": 7
+    }
+}
